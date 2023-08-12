63db69f3ec42e45e9077b0944693c7a3e640bce0
BNAS24 <124627177+BNAS24@users.noreply.github.com>
2023-08-12 17:04:34 -0400

    Title Text

--- src/Components/taskbar.js
+++ src/Components/taskbar.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './taskbar.css';
 
-const TaskBar = ({ className }) => (
+const TaskBar = ({ className, circDiv, circle, TaskTitle }) => (
     <div className={className}>
+        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 40 40" fill="none" className={circDiv}>
+            <circle cx="20" cy="20" r="18.5" fill="#979797" fillOpacity="0.16" stroke="#979797" strokeWidth="3" className={circle} />
+        </svg>
+
+        <div className='task-titleDiv'>
+            <h4 className='task-title'>
+                {TaskTitle}
+            </h4>
+        </div>
     </div>
 );
 
 export default TaskBar;
-
--- src/Components/taskbarGroup.js
+++ src/Components/taskbarGroup.js
@@ -4,15 +4,23 @@ import TaskBar from './taskbar';
 const TaskBarGroup = () => {
 
     const taskClass1 = "taskgroup";
-    const taskBar = "taskbar";
-    const taskBar1 = "taskbar1 taskbar";
+    const taskBar1 = "taskbar circle1 taskbar1";
+    const taskBar2 = "taskbar circle2";
+    const taskBar3 = "taskbar circle3";
+    const taskBar4 = "taskbar circle4";
+
+
+    const title1 = 'Task 4 kkk l eq'; /*only can allow 15 characters including spaces */
+    const title2 = 'Task 2';
+    const title3 = 'Task 3';
+    const title4 = 'Task 4';
 
     return (
         <div className={taskClass1}>
-            <TaskBar className={taskBar1} />
-            <TaskBar className={taskBar}/>
-            <TaskBar className={taskBar}/>
-            <TaskBar className={taskBar}/>
+            <TaskBar className={taskBar1} circle="circle1" circDiv="circleAll" TaskTitle={title1}/>
+            <TaskBar className={taskBar2} circle="circle2" circDiv="circleAll" TaskTitle={title2}/>
+            <TaskBar className={taskBar3} circle="circle3" circDiv="circleAll" TaskTitle={title3}/>
+            <TaskBar className={taskBar4} circle="circle4" circDiv="circleAll" TaskTitle={title4}/>
         </div>
     );
 };

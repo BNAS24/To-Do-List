d68b4932162d797532b16cda5983ab000937df16
BNAS24 <124627177+BNAS24@users.noreply.github.com>
2023-08-12 19:48:16 -0400

    necessary changes for compatibility

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import './App.css';
 import Heading from './Components/heading.js';
 import TaskBarGroup from './Components/taskbarGroup';
-import NewTask from './Components/newtask';
+
 
 function App() {
   return (
     <div className='appBody'>
       <Heading />
       <TaskBarGroup />
-      <NewTask />
+
     </div>
   );
 }

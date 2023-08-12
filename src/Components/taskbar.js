ea7d9da433d615ccf339b97676dd846fc90d5c6e
BNAS24 <124627177+BNAS24@users.noreply.github.com>
2023-08-12 17:59:37 -0400

    added due dates, paragraphs, and alignment adjustments

--- src/Components/taskbar.js
+++ src/Components/taskbar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './taskbar.css';
 
-const TaskBar = ({ className, circDiv, circle, TaskTitle }) => (
+const TaskBar = ({ className, circDiv, circle, TaskTitle, smallDescription }) => (
     <div className={className}>
         <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 40 40" fill="none" className={circDiv}>
             <circle cx="20" cy="20" r="18.5" fill="#979797" fillOpacity="0.16" stroke="#979797" strokeWidth="3" className={circle} />
@@ -12,6 +12,18 @@ const TaskBar = ({ className, circDiv, circle, TaskTitle }) => (
                 {TaskTitle}
             </h4>
         </div>
+        <div className='descriptionDiv'>
+            <p className='description'>
+                {smallDescription}
+            </p>
+        </div>
+        <div className='dateCreatedDiv'>
+    <p className='date-created'>Date Created</p> {/*The date will be dynamically  defined by a javascript function*/}
+        </div>
+        <div className='dueDateDiv'>
+    <p className='due-date'>Due Date</p> {/*The date will be dynamically  defined by a javascript function*/}
+        </div>
+
     </div>
 );
 
--- src/Components/taskbarGroup.js
+++ src/Components/taskbarGroup.js
@@ -15,12 +15,17 @@ const TaskBarGroup = () => {
     const title3 = 'Task 3';
     const title4 = 'Task 4';
 
+    const descrition1 = 'what the fuck is up'
+    const descrition2 = 'nothing much chicken butt'
+    const descrition3 = 'i cant complain im just out here maintaining, living my lifeNeque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...'
+    const descrition4 = 'This is also just some random text I picked up'
+
     return (
         <div className={taskClass1}>
-            <TaskBar className={taskBar1} circle="circle1" circDiv="circleAll" TaskTitle={title1}/>
-            <TaskBar className={taskBar2} circle="circle2" circDiv="circleAll" TaskTitle={title2}/>
-            <TaskBar className={taskBar3} circle="circle3" circDiv="circleAll" TaskTitle={title3}/>
-            <TaskBar className={taskBar4} circle="circle4" circDiv="circleAll" TaskTitle={title4}/>
+            <TaskBar className={taskBar1} circle="circle1" circDiv="circleAll" TaskTitle={title1} smallDescription={descrition1}/>
+            <TaskBar className={taskBar2} circle="circle2" circDiv="circleAll" TaskTitle={title2} smallDescription={descrition2}/>
+            <TaskBar className={taskBar3} circle="circle3" circDiv="circleAll" TaskTitle={title3} smallDescription={descrition3}/>
+            <TaskBar className={taskBar4} circle="circle4" circDiv="circleAll" TaskTitle={title4} smallDescription={descrition4}/>
         </div>
     );
 };

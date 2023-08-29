8f2c8fddaa0bf79e55773c1c04e46aebdcc653bf
BNAS24 <124627177+BNAS24@users.noreply.github.com>
2023-08-29 01:18:30 -0400

    position adjustments

--- src/Components/taskbarGroup.js
+++ src/Components/taskbarGroup.js
@@ -4,34 +4,28 @@ import { NewTask } from './newtask';
 import { useState } from 'react';
 
 const TaskBarGroup = () => {
+    const [tasks, setTasks] = useState([]);
 
-    const [tasks, setTasks] = useState([])
-
-    const month = new Date()
-    const day = new Date()
-    const year = new Date()
-    const date = `Created: ${month.getMonth()}/${day.getDay()}/${year.getFullYear()}`
+    const currentDate = new Date();
+    const formattedDate = `Created: ${currentDate.getMonth() + 1}/${currentDate.getDate()}/${currentDate.getFullYear()}`;
 
     const createTask = () => {
-
         const newTask = { 
             tasktitle: 'Task',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vulputate tristique leo, ut volutpat elit lobortis sit amet. Sed semper volutpat imperdiet. Donec lacinia sit amet est ut elementum. Morbi lorem nibh, eleifend quis imperdiet vel, mattis a neque.',
-            timestamp: date
-     }
-        setTasks([...tasks, newTask])
-
-    }
-
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+            timestamp: formattedDate
+        };
+        setTasks([...tasks, newTask]);
+    };
 
     return (
         <div className='taskgroup-wrapper'>
-        <div className="taskgroup">
-            {tasks.map((task, index) => (
-                <TaskBar key={index} taskData={task} />
-            ))}
-            <NewTask createTask={createTask} />
-        </div>
+            <div className="taskgroup">
+                {tasks.map((task, index) => (
+                    <TaskBar key={index} taskData={task} />
+                ))}
+                <NewTask createTask={createTask} />
+            </div>
         </div>
     );
 };

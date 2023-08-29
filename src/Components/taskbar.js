7691d2b5e3436cc8ec7ed5fea95ab946d7b0b4d2
BNAS24 <124627177+BNAS24@users.noreply.github.com>
2023-08-29 00:21:46 -0400

    modal toggle change

--- src/Components/taskbar.js
+++ src/Components/taskbar.js
@@ -7,13 +7,17 @@ const TaskBar = ({ taskData, Circle_Color }) => {
 
     const [isModalOpen, setIsModalOpen] = useState(false);
 
-    const toggleModal = () => {
+    const openModal = () => {
         setIsModalOpen(prevState => !prevState);
     };
 
+    const closeModal = () => {
+        setIsModalOpen(prevState => !prevState);
+    }
+
 
     return (
-        <div className="taskbar" onClick={toggleModal}>
+        <div className="taskbar" onClick={openModal}>
             <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 40 40" fill="none" className="circleAll">
                 <circle cx="20" cy="20" r="18.5" fill="#979797" fillOpacity="0.16" stroke="#979797" strokeWidth="3" className={Circle_Color} />
             </svg>
@@ -47,7 +51,7 @@ const TaskBar = ({ taskData, Circle_Color }) => {
 
             {/*Task Modal*/}
 
-            {isModalOpen && <TaskModal dateCreated={taskData.timestamp} onClick={toggleModal} />}
+            {isModalOpen && <TaskModal dateCreated={taskData.timestamp} onClick={closeModal} />}
 
         </div>
     );
